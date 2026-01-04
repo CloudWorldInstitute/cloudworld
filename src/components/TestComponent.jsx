16948d97f5bd9a0ba0ad198e2aa4a73722c73f6c
Mehraj7744 <mehrajpathan7744@gmail.com>
2026-01-04 18:58:08 +0530

    Deploy Vite build files for Hostinger

--- src/components/TestComponent.jsx
+++
@@ -1,4 +0,0 @@
-import './TestComponent.css';  // Create a simple TestComponent.css in the same folder
-
-const TestComponent = () => <div className="test-class">This is a test</div>;
-export default TestComponent;
