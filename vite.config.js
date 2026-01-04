96fa54f1573e126d9bc1241181a549084e8cdc42
Mehraj7744 <mehrajpathan7744@gmail.com>
2026-01-04 17:41:02 +0530

    cicd

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // 👈 important for Vercel deployments
+  base: "/", 
 })
 
